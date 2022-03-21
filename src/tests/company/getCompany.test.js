a7764fc1cfbc3ca2091b88fa3b85424f465953ad
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-03-21 18:30:23 -0300

    feat: test for valdiateSchema , one middleware and route register company

---
+++ src/tests/company/getCompany.test.js
@@ -0,0 +1,14 @@
+import { describe, expect, it } from "@jest/globals";
+import request from "supertest";
+
+import app from "../../app"
+
+
+describe("Test route GET /companies ", ()=>{
+    it("should be able return company list.", async()=>{
+        const response = await 
+        request(app).get("/companies");
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toStrictEqual([])
+    })
+});
